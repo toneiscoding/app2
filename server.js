35b8d2371a3793bd6e3e2b24c8cbb838702c5824
Anthoni Duong <aduong@LABA.PSWAS.LOCAL>
2023-06-15 16:19:02 -0700

    deleted _ from dirname filename

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 
-const _dirname = path.dirname(_filename);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
